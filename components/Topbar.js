cd8f2923beb9f4d153678e791cfb8789327b1acf
Mathias Dalla Palma <mathiasdallapalma@gmail.com>
2022-05-23 08:58:36 +0200

    Team Management pages

---
+++ components/Topbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Topbar.css"
+
+
+
+
+
+const Topbar = ()=>{
+    return (
+       <div className='Topbar'>
+           <img id="logo"></img>
+
+       </div> 
+    );
+}
+
+export default Topbar;
