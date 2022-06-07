cc54615b461c9ea5d7e4afec53f72944ea2f680e
pietrolechthaler <pietrolechthaler@gmail.com>
2022-06-07 10:59:06 +0200

    Update index.js PORT ADDRESS

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const port = process.env.PORT || 3001;
 
 /* --- DB Connection --- */
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true , useUnifiedTopology: true})
 .then(() => {
     console.log("Connected to database")
     /* --- Server Starting --- */
-    app.listen(3001, () => console.log("Server started"));
+    app.listen(port, () => console.log("Server started"));
 })
 .catch((error) => console.error("Error connecting to database: ${error}"));
 
